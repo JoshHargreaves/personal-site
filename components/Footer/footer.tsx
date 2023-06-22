2e71b34a8af5a347c8eda4644923978a311ed28d
Joshua Hargreaves <me@joshhargreaves.co.uk>
2023-06-22 20:23:25 +0100

    Update styles

--- components/Footer/footer.tsx
+++ components/Footer/footer.tsx
@@ -4,25 +4,6 @@ import { FaDiscord, FaGithub, FaLinkedin, FaMailchimp, FaRegEnvelope, FaTwitter 
 
 export const Footer = () => (
     <footer className="z-20 w-full p-16 bg-inherit shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-inherit dark:border-gray-600">
-        <span className="text-sm text-gray-500 sm:text-center dark:text-custom-primary">© 2023 Joshua Hargreaves
-        </span>
-        <ul className="flex flex-wrap items-center space-x-2  mt-3 text-sm font-medium md:text-custom-primary dark:text-custom-primary sm:mt-0">
-            <li>
-                <Link href="https://github.com/JoshHargreaves" className="mr-4 hover:underline md:mr-6">
-                    <FaGithub className="hover:text-violet-500 transition duration-300 ease-in-out " style={{ color: '#FFCACA', fontSize: '24px' }} />
-                </Link>
-            </li>
 
-            <li>
-                <Link href="https://www.linkedin.com/in/joshua-hargreaves-5b544b63/" className="mr-4 hover:underline md:mr-6">
-                    <FaLinkedin className="hover:text-violet-500 transition duration-300 ease-in-out " style={{ color: '#FFCACA', fontSize: '24px' }} />
-                </Link>
-            </li>
-            <li>
-                <Link href="mailto:me@joshhargreaves.co.uk" className="mr-4 hover:underline md:mr-6">
-                    <FaRegEnvelope className="hover:text-violet-500 transition duration-300 ease-in-out " style={{ color: '#FFCACA', fontSize: '24px' }} />
-                </Link>
-            </li>
-        </ul>
     </footer>
 )
--- components/Header/header.tsx
+++ components/Header/header.tsx
@@ -1,6 +1,33 @@
+import Link from "next/link"
+import { FaGithub, FaLinkedin, FaRegEnvelope } from "react-icons/fa"
+
 export const Header = () => {
-  return (
-    <nav className="bg-white border-gray-200 p-24 dark:bg-inherit">
-    </nav>
-  )
+    return (
+<nav className="bg-white border-gray-200 p-16 dark:bg-inherit">
+  <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+  <span className="text-sm text-gray-500 sm:text-center dark:text-custom-primary">Joshua Hargreaves
+        </span>
+    <div className="w-full md:block md:w-auto" id="navbar-default">
+    <ul className="flex flex-wrap items-center space-x-2  mt-3 text-sm font-medium md:text-custom-primary dark:text-custom-primary sm:mt-0">
+            <li>
+                <Link href="https://github.com/JoshHargreaves" className="mr-4 hover:underline md:mr-6">
+                    <FaGithub className="hover:text-violet-500 transition duration-300 ease-in-out " style={{ color: '#FFCACA', fontSize: '24px' }} />
+                </Link>
+            </li>
+
+            <li>
+                <Link href="https://www.linkedin.com/in/joshua-hargreaves-5b544b63/" className="mr-4 hover:underline md:mr-6">
+                    <FaLinkedin className="hover:text-violet-500 transition duration-300 ease-in-out " style={{ color: '#FFCACA', fontSize: '24px' }} />
+                </Link>
+            </li>
+            <li>
+                <Link href="mailto:me@joshhargreaves.co.uk" className="mr-4 hover:underline md:mr-6">
+                    <FaRegEnvelope className="hover:text-violet-500 transition duration-300 ease-in-out " style={{ color: '#FFCACA', fontSize: '24px' }} />
+                </Link>
+            </li>
+        </ul>
+    </div>
+  </div>
+</nav>
+    )
 }
